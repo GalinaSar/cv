a14e2f4c397b48e48c5a8777ebd312f2a877f2e4
Kazakova Galina <90353530+GalinaSar@users.noreply.github.com>
2022-10-23 15:58:20 +0400

    new

---
+++ js/app.js
@@ -0,0 +1,37 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const slides = document.querySelectorAll(".slide");
+    for (const slide of slides) slide.addEventListener("click", (() => {
+        clearActiveClasses();
+        slide.classList.add("active");
+    }));
+    function clearActiveClasses() {
+        slides.forEach((slide => {
+            slide.classList.remove("active");
+        }));
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
